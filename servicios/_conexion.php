f66c22abc29de685c440797a8f530427d2f91b2d
CesarIzquierdo <cesarizquierdo100@gmail.com>
2022-05-30 22:32:36 -0500

    first commit

---
+++ servicios/_conexion.php
@@ -0,0 +1,13 @@
+<?php
+	$con=mysqli_connect("localhost","root","","database");
+	
+	/*
+	$con=mysqli_connect('mysql:host=localhost','root','','database');
+	$con=mysqli_connect(
+	'ubicacion de la BD',
+	'usuario de la base de datos',
+	'contraseña del usuario de la base de datos',
+	'nombre] del esquema de base de datos');
+	*/
+?>
+<H1>CONECTADO</H1>
