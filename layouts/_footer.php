3a51fe81af40439e2eafab3485a2569f86bc216f
MosesHerrerus <105822925+MosesHerrerus@users.noreply.github.com>
2022-05-31 09:30:30 -0500

    Update _footer.php

--- layouts/_footer.php
+++ layouts/_footer.php
@@ -3,15 +3,15 @@
 		<div class="div-flex mb20">
 			<div class="part">
 				<h4>Quienes somos?</h4>
-				<p>Empresa dedicada al rubro de librerias y comercio electrónico</p>
+				<p>Somos una dulcería del estado de méxico y nos dedicamos a entregar y enviar dulces</p>
 			</div>
 			<div class="part">
 				<h4 class="mb10">Redes sociales</h4>
-				<a href="" target="_blank">Facebook: Mi tienda</a>
+				<a href="" target="_blank">Facebook: Tienda la Chida</a>
 				<br>
-				<a href="" target="_blank">Whatsapp: 999 888 777</a>
+				<a href="" target="_blank">Whatsapp: 1234567890</a>
 			</div>
 		</div>
-		<center><h5>Mi tienda - Derechos reservador 2021</h5></center>
+		<center><h5>Tienda la chida Est 2022</h5></center>
 	</div>
 </footer>
